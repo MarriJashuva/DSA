71940706242d0c4d112b2a549adba96bd67fab33
MarriJashuva <140377679+MarriJashuva@users.noreply.github.com>
2025-01-13 22:04:53 +0530

    Add files via upload

---
+++ Arrays/reversearrayusing_inbuiltin_functions.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Integer[] arr={1,3,4,3,5};
+        Collections.reverse(Arrays.asList(arr));
+        System.out.print(Arrays.toString(arr)); 
+    }
+}
